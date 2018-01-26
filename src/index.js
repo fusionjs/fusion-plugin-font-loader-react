3f3b4180377fc3df2903ecfa2efcb285d8fe0db5
Alexander Smith <amsmith@uber.com>
2018-01-25 16:11:19 -0800

    Migreate source to use DI and createPlugin API

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 // @flow
 export {default as withFontLoading} from './with-font-loading';
 export {default as FontPlugin} from './plugin';
+
+export {FontLoaderReactToken, FontLoaderReactConfigToken} from './tokens';
