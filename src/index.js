257d385d562894cd001882630d675715bdb195ac
Alexander Smith <amsmith@uber.com>
2018-01-30 14:41:08 -0800

    Explicit with import/re-export in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // @flow
-export {default as withFontLoading} from './with-font-loading';
-export {default} from './plugin';
 
-export {FontLoaderReactToken, FontLoaderReactConfigToken} from './tokens';
+import withFontLoading from './with-font-loading';
+import FontLoaderReactPlugin from './plugin';
+import {FontLoaderReactToken, FontLoaderReactConfigToken} from './tokens';
+
+export default FontLoaderReactPlugin;
+export {withFontLoading, FontLoaderReactToken, FontLoaderReactConfigToken};
